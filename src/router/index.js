429eec6a987d2a23c598308ced76a9dda94412a2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-01 09:15:51 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,18 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/about-me',
+      name: 'about-me',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../views/AboutMeView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/ProjectsView.vue')
+    },
   ]
 })
 
