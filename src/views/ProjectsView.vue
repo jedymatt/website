0f167e0481d2e7ffd8fa13be88f31a7c978afe51
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-01 16:41:00 +0800

    Update ProjectsView.vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import CardComponent from '../components/CardComponent.vue';
+import ProjectCardItem from '../components/ProjectCardItem.vue';
 </script>
 
 <template>
     <div class="p-4 grid grid-cols-1 gap-4">
-        <CardComponent title="umtc-election" description="" url="https://www.github.com/jedymatt/umtc-election" isPrivate="true" />
+        <ProjectCardItem title="umtc-election" description="" url="https://www.github.com/jedymatt/umtc-election"
+            isPrivate="true" />
     </div>
 </template>
