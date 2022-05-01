66db80ae835038c981dd4e36e3b4f52e80c1d66c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-01 16:38:35 +0800

    Create ProjectsView.vue

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CardComponent from '../components/CardComponent.vue';
+</script>
+
+<template>
+    <div class="p-4 grid grid-cols-1 gap-4">
+        <CardComponent title="umtc-election" description="" url="https://www.github.com/jedymatt/umtc-election" isPrivate="true" />
+    </div>
+</template>
