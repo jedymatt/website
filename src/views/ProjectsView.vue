cbaf87e75e35983ebe53e7204e5c9e82d77c6544
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-06 11:38:25 +0800

    Update ProjectsView.vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,7 +4,7 @@ import ProjectCardItem from '../components/ProjectCardItem.vue';
 
 <template>
     <div class="p-4 grid grid-cols-1 gap-4">
-        <ProjectCardItem title="umtc-election" description="" url="https://www.github.com/jedymatt/umtc-election"
-            isPrivate="true" />
+        <ProjectCardItem title="University of Mindanao Tagum College Election System" description=""
+            url="https://www.github.com/jedymatt/umtc-election" isPrivate="true" />
     </div>
 </template>
